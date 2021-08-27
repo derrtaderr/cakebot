7ab62c9b7b6d5adeedc1fd561a410d8e0be9b20f
Jason Derr <dharrmabum@gmail.com>
2021-08-26 23:20:30 -0700

    add sessions and all models to db

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ app = flask.Flask(__name__)
 app.register_blueprint(home.blueprint)
 app.register_blueprint(order_api.blueprint)
 
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
--- db/user.py
+++ db/user.py
@@ -1,14 +1,16 @@
 import datetime
 from typing import List
+
+from db.base_class import SqlAlchemyBase
 from db.order import Order
 
 
-class User:
-    id: int
-    created_data: datetime.datetime
+class User(SqlAlchemyBase):
+    id: int = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    created_data: datetime.datetime = sa.Column(sa.DATETIME, default=datetime.datetime.now, index=True)
 
-    name: str
-    phone: str
-    email: str
+    name: str = sa.Column(sa.String)
+    phone: str = sa.Column(sa.String, index=True)
+    email: str = sa.Column(sa.String, index=True)
 
-    orders: List[Order]
+    # orders: List[Order]
